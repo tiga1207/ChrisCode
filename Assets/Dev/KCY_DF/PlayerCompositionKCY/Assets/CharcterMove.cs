6ad5a5117fabc5775f82e48f37c3d17df1892397
ch-yun38 <kscy0328@gmail.com>
2025-04-28 17:52:36 +0900

    [feat] player basic move, attack, hp
    플레이어의 기본적인 이동, 공격,  체력 시스템 구현

---
+++ Assets/Dev/KCY_DF/PlayerCompositionKCY/Assets/CharcterMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    public float jumpPower = 1;
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+    }
+}
