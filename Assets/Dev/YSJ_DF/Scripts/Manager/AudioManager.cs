29a6f41fb8e6f69001c21e88bdb55a64ed66b5fe
DevIRU <deviru0413@gmail.com>
2025-04-30 12:50:21 +0900

    [feat] AudioManager 구현
    - 배경음악과 효과음 관련 관리 기능 구현

--- Assets/Dev/YSJ_DF/Scripts/Manager/AudioManager.cs
+++ Assets/Dev/YSJ_DF/Scripts/Manager/AudioManager.cs
@@ -1,23 +1,35 @@
 using Scripts.Interface;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Scripts.Manager
 {
     public class AudioManager : SimpleSingleton<AudioManager>, IManager
     {
+        private const string BGM_GAMEOBJECT_NAME = "BGM_Audio";
+        private const string SFX_GAMEOBJECT_NAME = "SFX_Audio";
+
+        private const float SOUND_MIN_VOLUME = 0.0f;
+        private const float SOUND_MAX_VOLUME = 1.0f;
+        
+        private const int SFX_POOL_SIZE = 5;
+
         private GameObject _audioManager;
 
         private AudioSource _bgmSource;
-        private AudioSource _fxSource;
+        private List<AudioSource> _sfxSources = new();
 
         // IManager
         public void Initialize()
         {
-            _bgmSource = gameObject.GetOrAddComponent<AudioSource>();
-            _bgmSource.loop = true;
+            _audioManager = this.gameObject;
 
-            _fxSource = gameObject.GetOrAddComponent<AudioSource>();
-            _bgmSource.loop = true;
+            // BGM
+            CreateBgm();
+            
+            // SFX
+            CreateSfxPool();
         }
         public void Cleanup()
         {
@@ -26,12 +38,33 @@ namespace Scripts.Manager
         {
             return this.gameObject;
         }
-        public void UpdateManager()
-        {
-
-        }
 
         // Audio
+        private void CreateBgm()
+        {
+            var bgm = Util.FindOrCreateGameObject(BGM_GAMEOBJECT_NAME);
+            bgm.transform.parent = _audioManager.transform;
+
+            _bgmSource = bgm.AddComponent<AudioSource>();
+            _bgmSource.loop = true;
+            _bgmSource.volume = 0.5f;
+        }
+        private void CreateSfxPool()
+        {
+            for (int i = 0; i < SFX_POOL_SIZE; i++)
+            {
+                var sfx = Util.FindOrCreateGameObject($"{SFX_GAMEOBJECT_NAME}_{i}");
+                sfx.transform.parent = _audioManager.transform;
+
+                var cmp = sfx.AddComponent<AudioSource>();
+                cmp.loop = false;
+                cmp.volume = 0.5f;
+
+                _sfxSources.Add(cmp);
+            }
+        }
+
+
         public void PlayBGM(AudioClip clip)
         {
             if (clip == null)
@@ -40,9 +73,48 @@ namespace Scripts.Manager
             _bgmSource.clip = clip;
             _bgmSource.Play();
         }
+        public void PlaySFX(AudioClip clip)
+        {
+            var source = _sfxSources.FirstOrDefault(s => !s.isPlaying);
+            if (source != null && clip != null)
+            {
+                source.clip = clip;
+                source.Play();
+            }
+        }
+
+
         public void StopBGM()
         {
             _bgmSource.Stop();
+        }
+        public void StopSFX()
+        {
+            foreach (var sfx in _sfxSources)
+                sfx.Stop();
+        }
+
+        // UI 쪽에서 사용하면 될 듯 합니다.(설정창에서 사용할 듯)
+        public void SetBgmVolume (float volume)
+        {
+            volume = Mathf.Clamp(volume, SOUND_MIN_VOLUME, SOUND_MAX_VOLUME);
+            _bgmSource.volume = volume;
+        }
+        public void SetSfxVolume(float volume)
+        {
+            volume = Mathf.Clamp(volume, SOUND_MIN_VOLUME, SOUND_MAX_VOLUME);
+            foreach (var sfx in _sfxSources)
+                sfx.volume = volume;
+        }
+
+        public void MuteBgm()
+        {
+            _bgmSource.volume = SOUND_MIN_VOLUME;
+        }
+        public void MuteSfx()
+        {
+            foreach (var sfx in _sfxSources)
+                sfx.volume = SOUND_MIN_VOLUME;
         }
     }
 }
