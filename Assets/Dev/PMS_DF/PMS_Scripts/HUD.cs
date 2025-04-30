903391875e3812eb21a0b9cd39faee34fd568488
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-04-30 14:57:45 +0900

    [Feat] HUB UI코드 수정
    HUD 스크립트에서 타이머가 00:00에서 멈춰 마이너스 값을 가지지 않도록 수정
    
    이전 UIManager 코드 분석 및 테스트 진행

--- Assets/Dev/PMS_DF/PMS_Scripts/HUD.cs
+++ Assets/Dev/PMS_DF/PMS_Scripts/HUD.cs
@@ -17,8 +17,8 @@ public class HUD : MonoBehaviour
     int level = 999;
     int kill = 100;
 
-    public float maxTime = 150;  //
-    public float currentTime = 30;
+    private float maxTime = 150;  //
+    private float currentTime = 160;
     public float remainTime;
 
     private void Awake()
@@ -36,18 +36,28 @@ public class HUD : MonoBehaviour
                 float maxExp = 10;  //TODO - 다음 레벨 경험치 가져오기
                 mySlider.value = curExp / maxExp;
                 break;
+
             case InfoType.Level:
                 myText.text = string.Format("Lv.{0:F0}", level);    //TODO - 현재 레벨 불러오기
                 break;
+
             case InfoType.Kill:
                 myText.text = string.Format("{0:F0}", kill);    //TODO - 현재 레벨 불러오기
                 break;
+
             case InfoType.Time:
                 remainTime = maxTime - currentTime;             //TODO -  값 설정 나중에
                 int min = Mathf.FloorToInt(remainTime / 60);
                 int sec = Mathf.FloorToInt(remainTime % 60);
-                myText.text = string.Format("{0:D2}:{1:D2}", min,sec);    
+                Debug.Log(remainTime);
+                if (min >= 0 && sec >= 0)
+                    myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
+                else
+                {
+                    myText.text = "00:00"; //더이상 시간이 흐르지 않게
+                }
                 break;
+
             case InfoType.Health:
                 break;
         }
