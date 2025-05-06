65b55c73d7e298230adfb87226ebac3201a2f442
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-05-06 10:57:40 +0900

    [Feat] 스킬 선택 UI 스크립트 제작
    스킬 선택 UI 스크립트 제작 (캐릭터 스킬 불러오는것에는 미연동)

--- Assets/Dev/PMS_DF/PMS_Scripts/SkillSelectionUI.cs
+++ Assets/Dev/PMS_DF/PMS_Scripts/SkillSelectionUI.cs
@@ -1,11 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillSelectionUI : MonoBehaviour
 {
     [Header("SKill UI Settings")]
-    [SerializeField] private GameObject[] skillSlot;
+    [SerializeField] private Image[] icon;
+    [SerializeField] private TextMeshProUGUI[] name;
+    [SerializeField] private TextMeshProUGUI[] description;
 
+    private PMS_Skill firstSkill;
+
+    public Button[] playerSkillSelectButton;
     // TODO - 플레이어 스킬을 어떻게 들고 올 것인지
+
+
+    private void Awake()
+    {
+        //스킬 3가지 랜덤 제공만 해놓으면 해당 데이터를 Awake()에서 받아서
+
+        //임시 스킬 생성
+        firstSkill = new PMS_Skill("FireBall", "A powerful fireball comes out ahead..");
+        //현재 아이콘 x
+        //icon = firstSkill.icon;
+
+        name[0].text = firstSkill.skillName;
+        description[0].text = firstSkill.skillDescription;
+        
+    }
+
+    private void Start()
+    {
+        //각 버튼 이벤트 활성화해주고
+        for(int i = 0; i < 1; i++)
+        {
+            playerSkillSelectButton[i].onClick.AddListener(OnSkillSelectSlot);     
+        }
+    }
+
+    private void OnSkillSelectSlot()
+    {
+        //누르면 해당 스킬을 선택한것을 캐릭터에게 반환
+    }
 }
