a041ed35af35cdff17cd9fd1cd6db9777f160211
DevIRU <deviru0413@gmail.com>
2025-05-07 12:16:51 +0900

    [feat] AudioManager 사운드 볼륨 관련 수정
    - 여러 sfx를 사운드 관리를 위해서 sfx들 볼륨 관리를 할 변수를 생성 및 함수 추가

--- Assets/Dev/YSJ_DF/Scripts/Manager/AudioManager.cs
+++ Assets/Dev/YSJ_DF/Scripts/Manager/AudioManager.cs
@@ -7,75 +7,56 @@ namespace Scripts.Manager
 {
     public class AudioManager : SimpleSingleton<AudioManager>, IManager
     {
+        #region Constants
         private const string BGM_GAMEOBJECT_NAME = "BGM_Audio";
         private const string SFX_GAMEOBJECT_NAME = "SFX_Audio";
 
         private const float SOUND_MIN_VOLUME = 0.0f;
         private const float SOUND_MAX_VOLUME = 1.0f;
-        
+
         private const int SFX_POOL_SIZE = 5;
+        #endregion
 
-        private GameObject _audioManager;
+        #region PrivateVariables
+        private GameObject m_audioManager;
+        private AudioSource m_bgmSource;
+        private List<AudioSource> m_sfxSources = new();
 
-        private AudioSource _bgmSource;
-        private List<AudioSource> _sfxSources = new();
+        private float m_sfxVolume = 1.0f;
+        #endregion
 
-        // IManager
+        #region PublicVariables
+        public int Priority => (int)ManagerPriority.AudioManager;
+        #endregion
+
+        #region PublicMethod
         public void Initialize()
         {
-            _audioManager = this.gameObject;
+            m_audioManager = gameObject;
 
-            // BGM
             CreateBgm();
-            
-            // SFX
             CreateSfxPool();
         }
-        public void Cleanup()
-        {
-        }
+
+        public void Cleanup() { }
+
         public GameObject GetGameObject()
         {
-            return this.gameObject;
+            return gameObject;
         }
 
-        // Audio
-        private void CreateBgm()
-        {
-            var bgm = Util.FindOrCreateGameObject(BGM_GAMEOBJECT_NAME);
-            bgm.transform.parent = _audioManager.transform;
-
-            _bgmSource = bgm.AddComponent<AudioSource>();
-            _bgmSource.loop = true;
-            _bgmSource.volume = 0.5f;
-        }
-        private void CreateSfxPool()
-        {
-            for (int i = 0; i < SFX_POOL_SIZE; i++)
-            {
-                var sfx = Util.FindOrCreateGameObject($"{SFX_GAMEOBJECT_NAME}_{i}");
-                sfx.transform.parent = _audioManager.transform;
-
-                var cmp = sfx.AddComponent<AudioSource>();
-                cmp.loop = false;
-                cmp.volume = 0.5f;
-
-                _sfxSources.Add(cmp);
-            }
-        }
-
-
-        public void PlayBGM(AudioClip clip)
+        public void PlayBgm(AudioClip clip)
         {
             if (clip == null)
                 return;
 
-            _bgmSource.clip = clip;
-            _bgmSource.Play();
+            m_bgmSource.clip = clip;
+            m_bgmSource.Play();
         }
+
         public void PlaySFX(AudioClip clip)
         {
-            var source = _sfxSources.FirstOrDefault(s => !s.isPlaying);
+            var source = m_sfxSources.FirstOrDefault(s => !s.isPlaying);
             if (source != null && clip != null)
             {
                 source.clip = clip;
@@ -83,38 +64,79 @@ namespace Scripts.Manager
             }
         }
 
-
-        public void StopBGM()
+        public void StopBgm()
         {
-            _bgmSource.Stop();
+            m_bgmSource.Stop();
         }
-        public void StopSFX()
+
+        public void StopSfx()
         {
-            foreach (var sfx in _sfxSources)
+            foreach (var sfx in m_sfxSources)
                 sfx.Stop();
         }
 
-        // UI 쪽에서 사용하면 될 듯 합니다.(설정창에서 사용할 듯)
-        public void SetBgmVolume (float volume)
+        public void SetBgmVolume(float volume)
         {
             volume = Mathf.Clamp(volume, SOUND_MIN_VOLUME, SOUND_MAX_VOLUME);
-            _bgmSource.volume = volume;
+            m_bgmSource.volume = volume;
         }
+
         public void SetSfxVolume(float volume)
         {
             volume = Mathf.Clamp(volume, SOUND_MIN_VOLUME, SOUND_MAX_VOLUME);
-            foreach (var sfx in _sfxSources)
-                sfx.volume = volume;
+            m_sfxVolume = volume;
+            foreach (var sfx in m_sfxSources)
+                sfx.volume = m_sfxVolume;
+        }
+
+        public float GetBgmVolume()
+        {
+            return m_bgmSource.volume;
+        }
+        public float GetSfxVolume()
+        {
+            return m_sfxVolume;
         }
 
         public void MuteBgm()
         {
-            _bgmSource.volume = SOUND_MIN_VOLUME;
+            m_bgmSource.volume = SOUND_MIN_VOLUME;
         }
+
         public void MuteSfx()
         {
-            foreach (var sfx in _sfxSources)
+            foreach (var sfx in m_sfxSources)
                 sfx.volume = SOUND_MIN_VOLUME;
         }
+        #endregion
+
+        #region PrivateMethod
+        private void CreateBgm()
+        {
+            GameObject bgm = Util.FindOrCreateGameObject(BGM_GAMEOBJECT_NAME);
+            bgm.transform.parent = m_audioManager.transform;
+
+            m_bgmSource = bgm.AddComponent<AudioSource>();
+            m_bgmSource.loop = true;
+            m_bgmSource.volume = 0.5f;
+        }
+
+        private void CreateSfxPool()
+        {
+            for (int i = 0; i < SFX_POOL_SIZE; i++)
+            {
+                GameObject sfx = Util.FindOrCreateGameObject($"{SFX_GAMEOBJECT_NAME}_{i}");
+                sfx.transform.parent = m_audioManager.transform;
+
+                AudioSource cmp = sfx.AddComponent<AudioSource>();
+                cmp.loop = false;
+                cmp.volume = 0.5f;
+
+                m_sfxSources.Add(cmp);
+            }
+
+            m_sfxVolume = 0.5f;
+        }
+        #endregion
     }
 }
